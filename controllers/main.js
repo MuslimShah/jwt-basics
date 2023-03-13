71637f35185ea74006f0c7168f8451eb31364dd9
Muslim shah <smuslimshah7@gmail.com>
2023-03-14 00:17:18 +0500

    setting up jwt token

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,25 @@
+const jwt = require('jsonwebtoken');
+
 const CustomApiError = require('../errors/custom-error');
 const login = async(req, res, next) => {
     const { username, password } = req.body;
+    console.log(username);
+    console.log(password);
     if (!username || !password) {
         throw new CustomApiError('please provide username and password', 400)
     }
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, `${process.env.JWT_SECRET_KEY}`, { expiresIn: '30d' });
 
-    res.send('fake login route');
+    res.status(200).json({ msg: 'user created', token });
 }
 
 const dashboard = async(req, res, next) => {
+    console.log(req.headers);
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new CustomApiError('no token provided', 400);
+    }
     let luckynumber = (Math.random() * 100);
     res.status(200).json({ msg: `hello syed`, secret: `here is your secret key ${luckynumber}` })
 }
